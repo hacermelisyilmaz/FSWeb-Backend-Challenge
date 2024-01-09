eafebfad65c6e64cbdd627c6e8b0e0635ab47b30
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-09 22:57:58 +0300

    Validations added

--- src/main/java/com/workintech/ecommerce/entity/Category.java
+++ src/main/java/com/workintech/ecommerce/entity/Category.java
@@ -3,12 +3,14 @@ package com.workintech.ecommerce.entity;
 import com.workintech.ecommerce.util.Gender;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -22,21 +24,24 @@ public class Category {
     private Integer id;
 
     @Column(name = "title")
-    @NotBlank
+    @NotBlank(message = "Invalid category title.")
     @Size(max = 20, message = "Maximum character limit 20 for category title is exceeded.")
     private String title;
 
     @Column(name = "code")
-    @NotBlank
+    @NotBlank(message = "Invalid category code.")
     @Size(max = 15, message = "Maximum character limit 15 for category code is exceeded.")
     private String code;
 
     @Enumerated(EnumType.STRING)
-    @NotBlank
+    @NotBlank(message = "Invalid category gender.")
     private Gender gender;
 
     @Column(name = "img")
-    @NotBlank
-    @URL
+    @NotBlank(message = "Invalid category image.")
+    @URL(message = "Invalid category image URL.")
     private String img;
+
+    @OneToMany(cascade = {CascadeType.ALL}, mappedBy = "category")
+    private List<Product> products = new ArrayList<>();
 }
