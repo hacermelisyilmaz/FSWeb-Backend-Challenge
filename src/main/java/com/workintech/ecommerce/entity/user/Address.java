74aefc7056b991a6cd6fd5d9b1eef639f9233ecf
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-09 23:19:45 +0300

    Variables declared with validations

---
+++ src/main/java/com/workintech/ecommerce/entity/user/Address.java
@@ -0,0 +1,59 @@
+package com.workintech.ecommerce.entity.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "address", schema = "public")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    @NotBlank(message = "Address title cannot be empty.")
+    @Size(max = 20, message = "Maximum character limit for address title (20) is exceeded.")
+    private String title;
+
+    @Column(name = "name")
+    @NotBlank(message = "Name cannot be empty.")
+    @Size(max = 20, message = "Maximum character limit for name (20) is exceeded.")
+    private String name;
+
+    @Column(name = "surname")
+    @NotBlank(message = "Surname cannot be empty.")
+    @Size(max = 20, message = "Maximum character limit for surname (20) is exceeded.")
+    private String surname;
+
+    @Column(name = "phone")
+    @NotBlank(message = "Phone cannot be empty.")
+    @Size(max = 15, message = "Maximum character limit for phone (15) is exceeded.")
+    private String phone;
+
+    @Column(name = "city")
+    @NotBlank(message = "City cannot be empty.")
+    @Size(max = 20, message = "Maximum character limit for city (20) is exceeded.")
+    private String city;
+
+    @Column(name = "district")
+    @NotBlank(message = "District cannot be empty.")
+    @Size(max = 30, message = "Maximum character limit for district (30) is exceeded.")
+    private String district;
+
+    @Column(name = "neighborhood")
+    @NotBlank(message = "Neighborhood cannot be empty.")
+    @Size(max = 50, message = "Maximum character limit for neighborhood (20) is exceeded.")
+    private String neighborhood;
+
+    @Column(name = "address")
+    @NotBlank(message = "Address details cannot be empty.")
+    private String address;
+}
---
+++ src/main/java/com/workintech/ecommerce/entity/user/Role.java
@@ -0,0 +1,31 @@
+package com.workintech.ecommerce.entity.user;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "role", schema = "public")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "authority")
+    @NotBlank(message = "Role name is invalid.")
+    @Size(max = 10, message = "Maximum character limit (10) for the role name is exceeded.")
+    private String authority;
+
+    @Column(name = "code")
+    @NotBlank(message = "Role code is invalid.")
+    @Size(max = 10, message = "Maximum character limit (10) for the role code is exceeded.")
+    private String code;
+}
