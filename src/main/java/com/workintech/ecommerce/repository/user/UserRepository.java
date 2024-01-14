1f6bbfa09ea557ceca0a9996033c7ba762aef105
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-14 19:54:56 +0300

    Repo. layer created, extended w/ JpaRepository. Query method added

---
+++ src/main/java/com/workintech/ecommerce/repository/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.workintech.ecommerce.repository.user;
+
+import com.workintech.ecommerce.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findUserByEmail(String email);
+}
