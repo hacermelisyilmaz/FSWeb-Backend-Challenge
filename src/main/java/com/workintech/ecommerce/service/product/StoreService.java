eba34e410ecb06c4ad5ae2494b8abc393cfc461b
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-25 22:58:58 +0300

    Service layer created with get method

---
+++ src/main/java/com/workintech/ecommerce/service/product/StoreService.java
@@ -0,0 +1,7 @@
+package com.workintech.ecommerce.service.product;
+
+import com.workintech.ecommerce.entity.product.Store;
+
+public interface StoreService {
+    Store getStore(Integer id);
+}
---
+++ src/main/java/com/workintech/ecommerce/service/user/RoleService.java
@@ -0,0 +1,9 @@
+package com.workintech.ecommerce.service.user;
+
+import com.workintech.ecommerce.entity.user.Role;
+
+import java.util.List;
+
+public interface RoleService {
+    List<Role> getRoles();
+}
