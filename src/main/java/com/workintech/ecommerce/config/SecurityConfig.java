2a8e4b780478d50d2198d85318353dfe11f35008
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-25 23:00:45 +0300

    Security configuration

---
+++ src/main/java/com/workintech/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.workintech.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(daoAuthenticationProvider);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth/**").permitAll();
+                    /*auth.requestMatchers(HttpMethod.GET, "/accounts/**").hasAnyAuthority("ADMIN", "USER");
+                    auth.requestMatchers(HttpMethod.POST, "/accounts/**").hasAuthority("ADMIN");
+                    auth.requestMatchers(HttpMethod.PUT, "/accounts/**").hasAuthority("ADMIN");
+                    auth.requestMatchers(HttpMethod.DELETE, "/accounts/**").hasAuthority("ADMIN");*/
+                    auth.requestMatchers("/products/**").permitAll();
+                    auth.requestMatchers("/admin/**").hasAuthority("admin");
+                    auth.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}
