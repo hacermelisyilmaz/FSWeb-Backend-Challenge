2b32d3bae1699c02781fa55979702fb278bb55fb
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-25 23:00:06 +0300

    Queries added

--- src/main/java/com/workintech/ecommerce/repository/product/ProductRepository.java
+++ src/main/java/com/workintech/ecommerce/repository/product/ProductRepository.java
@@ -2,6 +2,15 @@ package com.workintech.ecommerce.repository.product;
 
 import com.workintech.ecommerce.entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT * FROM public.product WHERE category_id = :categoryId", nativeQuery = true)
+    List<Product> findByCategoryId(@Param("categoryId") Integer categoryId);
+
+    @Query(value = "SELECT COUNT(product_id) FROM public.product", nativeQuery = true)
+    Integer getTotalProductNumber();
 }
