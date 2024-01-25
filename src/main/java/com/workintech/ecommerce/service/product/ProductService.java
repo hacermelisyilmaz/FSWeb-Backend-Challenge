8babf6fe1cca5c9de1d47bcd073dd94d11433255
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2024-01-25 22:58:25 +0300

    DTOs removed from service layer. Additional methods

--- src/main/java/com/workintech/ecommerce/service/product/ProductService.java
+++ src/main/java/com/workintech/ecommerce/service/product/ProductService.java
@@ -1,10 +1,20 @@
 package com.workintech.ecommerce.service.product;
 
-import com.workintech.ecommerce.dto.product.ProductResponse;
+import com.workintech.ecommerce.entity.product.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    List<ProductResponse> getProducts();
-    ProductResponse getProduct(Long id);
+    List<Product> getProducts();
+    Product getProduct(Long id);
+    Integer getTotalProductNumber();
+
+    Product save(Product product);
+    List<Product> saveAll(List<Product> products);
+
+    Product delete(Long id);
+
+    List<Product> findByCategoryId(Integer categoryId);
+
+    List<Product> findByRequestParams(Integer categoryId, String sort, String filter);
 }
